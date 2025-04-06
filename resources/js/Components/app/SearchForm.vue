ddcbef01542855feae23f0e36fca42cdc1aed62c
DrakLionRRR <lllevaa28@gmail.com>
2025-04-06 11:16:36 +0300

    add files models with migrations, start common front end

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput
+            v-model="form.search"
+            autocomplete
+            class="block w-full mr-2"
+            placeholder="Search for files and folders"
+            type="text"
+        />
+    </form>
+</template>
+
+<script setup>
+import {useForm} from "@inertiajs/vue3";
+import TextInput from "@/Components/TextInput.vue";
+
+const form = useForm({
+    search: ''
+})
+</script>
